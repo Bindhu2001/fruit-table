5b7b548c35f1d8bc8f63c1f3433304db00dab52e
Bindhu2001 <bindhuvijayan01@gmail.com>
2024-03-22 12:45:49 +0530

    script page

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+        $('#btn').click(function(){
+            var value =$('#select').val();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+            if(value =='apple'){
+                var tdcol = document.getElementById("apple");
+                tdcol.style.background ="red";
+                
+            }
+            else {
+                var tdcol = document.getElementById("apple");
+                tdcol.style.background ="white";
+            }
+
+            if(value =='orange'){
+                var tdcol = document.getElementById("orange");
+                tdcol.style.background ="orange";
+            }
+            else {
+                var tdcol = document.getElementById("orange");
+                tdcol.style.background ="white";
+            }
+
+            if(value =='mango'){
+                var tdcol = document.getElementById("mango");
+                tdcol.style.background ="yellow";
+            }
+            else {
+                var tdcol = document.getElementById("mango");
+                tdcol.style.background ="white";
+            }
+
+            if(value =='strawberry'){
+                var tdcol = document.getElementById("strawberry");
+                tdcol.style.background ="pink";
+            }
+            else {
+                var tdcol = document.getElementById("strawberry");
+                tdcol.style.background ="white";
+            }
+
+            if(value =='pineapple'){
+                var tdcol = document.getElementById("pineapple");
+                tdcol.style.background ="yellow";
+            }
+            else {
+                var tdcol = document.getElementById("pineapple");
+                tdcol.style.background ="white";
+            }
+
+            if(value =='guava'){
+                var tdcol = document.getElementById("guava");
+                tdcol.style.background ="green";
+            }
+            else {
+                var tdcol = document.getElementById("guava");
+                tdcol.style.background ="white";
+            }
+            if(value =='grapes'){
+                var tdcol = document.getElementById("grapes");
+                tdcol.style.background ="purple";
+            }
+            else {
+                var tdcol = document.getElementById("grapes");
+                tdcol.style.background ="white";
+            }
+
+        });       
+    });
