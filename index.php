700e4ea5e459ba54dff0f988f1287e2f6d089fca
Bindhu2001 <bindhuvijayan01@gmail.com>
2024-03-22 12:29:32 +0530

    index page

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+   
+    <title>search</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="fruit-names">
+        <select id="select">
+          <option value="" selected disabled> fruit search</option> 
+            <option value="apple">Apple</option> 
+            <option value="orange">Orange</option> 
+            <option value="mango">Mango</option> 
+            <option value="strawberry">strawberry</option> 
+            <option value="pineapple">Pineapple</option> 
+            <option value="guava">Guava</option> 
+            <option value="grapes">Grapes</option> 
+        </select> 
+
+
+    </div>
+     <BR><BR>
+<table>
+  <thead>
+    <tr>
+      <th>Position</th>
+      <th>Fruit</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td id="apple">Apple</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td id="orange">Orange</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td id="mango">Mango</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td id="strawberry">Strawberry</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td id="pineapple">Pineapple</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td id="guava">Guava</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td id="grapes">Grapes</td>
+    </tr>
+  </tbody>
+</table>
+<button type="button" id="btn"> connect </button>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script> 
+<script src="stript.js"></script>
+</script>
+</body>
+</html>
